ccfed33dfd36fa77fbb3187fe1dcdd4adb9ed43f
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-09-26 13:49:08 +0300

    refactor

--- packages/spiritswap-example/src/App.tsx
+++ packages/spiritswap-example/src/App.tsx
@@ -4,24 +4,26 @@ import { useWeb3React } from "@web3-react/core";
 import { injectedConnector } from "./connectors";
 import TWAP_Spiritswap from "@orbs-network/twap-ui-spiritswap";
 
-function App() {
-  const { activate, deactivate, account, library } = useWeb3React();
+function ConnectBtn() {
+  const { activate, deactivate, account } = useWeb3React();
 
+  const disconnect = <button onClick={deactivate}>Disconnect {account}</button>;
+  const connect = <button onClick={() => activate(injectedConnector)}>Connect</button>;
+
+  return <div>{account ? disconnect : connect}</div>;
+}
+
+function App() {
   return (
     <StyledApp className="App">
-      {ConnectBtn(activate, deactivate, account || "")}
-      <TWAP_Spiritswap provider={library} />
+      <ConnectBtn />
+
+      <TWAP_Spiritswap provider={useWeb3React().library} />
     </StyledApp>
   );
 }
 
 export default App;
-
-function ConnectBtn(activate: (connector: any) => void, deactivate: () => void, account: string) {
-  const disconnect = <button onClick={deactivate}>Disconnect {account}</button>;
-  const connect = <button onClick={() => activate(injectedConnector)}>Connect</button>;
-  return <div>{account ? disconnect : connect}</div>;
-}
 
 const StyledApp = styled(Box)({
   display: "flex",
