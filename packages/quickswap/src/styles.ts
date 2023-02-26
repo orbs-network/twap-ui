9c78dd77ef2316d71734ce4af7225b239136c7ca
antongotech <105588893+antongotech@users.noreply.github.com>
2023-02-26 16:45:46 +0200

    Quickswap's confirmation popup styles changed

--- packages/quickswap/src/styles.ts
+++ packages/quickswap/src/styles.ts
@@ -104,14 +104,13 @@ export const configureStyles = () => {
       },
     },
     ".twap-limit-price-input": {
+      background: "#1B1E29",
+      padding: "5px 10px!important",
       button: {
         padding: 0,
       },
       justifyContent: "space-between!important",
       width: "100%",
-      paddingTop: 10,
-      paddingBottom: 10,
-      paddingLeft: "0!important",
       borderRadius: 10,
       gap: 10,
       ".twap-input": {
@@ -532,7 +531,7 @@ export const configureStyles = () => {
       width: "calc(100% - 52px)!important",
       borderRadius: 10,
       background: styles.buttonBackground,
-      color: styles.textColor,
+      color: "white",
       fontWeight: `500!important`,
       fontSize: 16,
       margin: "0 26px",
@@ -548,12 +547,38 @@ export const configureStyles = () => {
     },
 
     ".twap-modal-content": {
+      fontSize: "14px",
+      fontFamily: "Inter",
+      maxWidth: "454px!important",
+      boxSizing: "border-box",
       background: styles.containerBackground,
 
+      ".twap-orders-summary-token-display": {
+        ".twap-token-logo": {
+          width: 30,
+          height: 30,
+        },
+        ".twap-orders-summary-token-display-amount": {
+          fontSize: 16,
+        },
+      },
+      ".twap-order-summary-details": {
+        ".twap-order-summary-details-item": {
+          div: {
+            fontSize: 14,
+          },
+          ".twap-label": {
+            fontSize: 16,
+          },
+        },
+      },
+      ".twap-disclaimer-text p, .twap-disclaimer-text a": {
+        fontSize: "14px",
+      },
       maxHeight: "85vh",
       overflow: "auto",
-      borderRadius: "10px",
-      padding: 15,
+      borderRadius: "24px",
+      padding: "24px 0",
       paddingTop: 30,
       color: styles.textColor,
       "& a": {
@@ -567,6 +592,9 @@ export const configureStyles = () => {
       "& *": {
         fontFamily: "inherit",
         color: "inherit",
+      },
+      ".twap-order-summary-limit-price": {
+        padding: "10px 26px",
       },
     },
     ".twap-change-tokens-order": {
