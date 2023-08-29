d4aa157fd873cc6e434151c85bea00e56aa579ea
DanielZlotin <danielzlotin@users.noreply.github.com>
2023-08-29 16:44:58 +0300

    update

--- packages/lib/test/fixture.ts
+++ packages/lib/test/fixture.ts
@@ -1,4 +1,4 @@
-import { erc20s, setWeb3Instance, Token as cToken, web3, network } from "@defi.org/web3-candies";
+import { erc20s, setWeb3Instance, Token as cToken, web3, network, erc20sData } from "@defi.org/web3-candies";
 import { configure } from "@testing-library/react";
 import { useChaiBigNumber } from "@defi.org/web3-candies/dist/hardhat";
 import type { TokenData } from "@orbs-network/twap";
@@ -20,17 +20,6 @@ export async function initFixture() {
   setWeb3Instance(new Web3(networkUrl!));
   configure({ asyncUtilTimeout: 10_000 });
 
-  tokens = await baseTokens();
+  tokens = (erc20sData as any)[network(CHAIN_ID)!.shortname] as TokenData[];
   maker = web3().eth.accounts.create().address;
-}
-
-async function baseTokens() {
-  const tokens = _.get(erc20s, [network(CHAIN_ID)!.shortname]);
-
-  return Promise.all(
-    _.map(tokens, async (token: () => cToken) => {
-      const t = token();
-      return { decimals: await t.decimals(), symbol: t.name, address: t.address } as TokenData;
-    })
-  );
 }
